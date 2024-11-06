48532a3007028523758d901934ee809da83b3826
Anuar7777 <aman_anuar@mail.ru>
2024-11-06 19:43:28 +0500

    Success Deploy Web

---
+++ app/questions/dist/Component-dbg.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("questions.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
